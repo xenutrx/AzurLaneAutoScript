f2fc2f623c564b9e9344a2d96981777c5d176694
wess09 <zicheng_1202@outlook.com>
2026-03-19 21:21:09 +0900

    fix

--- campaign/campaign_main/campaign_16_4.py
+++ campaign/campaign_main/campaign_16_4.py
@@ -1,39 +1,50 @@
-from module.logger import logger
 from module.campaign.campaign_base import CampaignBase
+from module.map.map_base import CampaignMap
 from module.map.map_grids import SelectedGrids, RoadGrids
-
-from .campaign_16_base import CampaignBase, CampaignMap
-from .campaign_16_base import Config as ConfigBase
+from module.logger import logger
+from campaign_16_1 import Config as ConfigBase
 
 MAP = CampaignMap('16-4')
 MAP.shape = 'K8'
-MAP.camera_data = ['C2', 'C6', 'F2', 'F6', 'H2', 'H6']
-MAP.camera_data_spawn_point = ['C6']
-MAP.camera_sight = (-2, -1, 3, 2)
+MAP.camera_data = ['D2', 'D6', 'H2', 'H6']
+MAP.camera_data_spawn_point = ['D6']
 MAP.map_data = """
-    -- -- ++ -- -- -- ++ ME -- -- MB
-    ME ++ ++ ++ -- -- ME ++ -- -- --
-    -- -- ME -- -- ++ ++ ME -- -- --
-    -- -- -- ME ++ -- ME -- ++ ++ --
-    -- -- ME -- -- ME ++ -- ME ++ --
-    -- __ -- ++ ++ -- ++ ME ME -- --
-    SP -- -- ME -- -- ME ++ -- ++ ++
+    -- -- ++ -- -- -- ++ -- -- -- MB
+    -- ++ ++ ++ -- -- -- ++ -- -- --
+    -- -- -- -- -- ++ ++ -- -- -- --
+    -- -- -- -- ++ -- -- -- ++ ++ --
+    -- -- -- -- -- -- ++ -- -- ++ --
+    -- __ -- ++ ++ -- ++ -- -- -- --
+    SP -- -- -- -- -- -- ++ -- ++ ++
     SP -- -- -- ++ -- ++ ++ -- -- ++
 """
 MAP.weight_data = """
     50 50 50 50 50 50 50 50 50 50 50
     50 50 50 50 50 50 50 50 50 50 50
-    50 50 50 50 50 50 50 40 50 50 50
-    50 50 50 40 50 40 40 40 50 50 50
-    50 50 50 40 40 40 50 50 50 50 50
+    50 50 50 50 50 50 50 50 50 50 50
+    50 50 50 50 50 50 50 50 50 50 50
+    50 50 50 50 50 50 50 50 50 50 50
     50 50 50 50 50 50 50 50 50 50 50
     50 50 50 50 50 50 50 50 50 50 50
     50 50 50 50 50 50 50 50 50 50 50
 """
 MAP.spawn_data = [
-    {'battle': 0, 'enemy': 5},
-    {'battle': 1, 'enemy': 4},
-    {'battle': 2, 'enemy': 5},
+    {'battle': 0, 'mystery': 2},
+    {'battle': 1},
+    {'battle': 2},
+    {'battle': 3},
+    {'battle': 4},
+    {'battle': 5},
+    {'battle': 6},
+    {'battle': 7},
+    {'battle': 8},
+    {'battle': 9},
+    {'battle': 10, 'boss': 1},
+]
+MAP.spawn_data_loop = [
+    {'battle': 0, 'mystery': 2},
+    {'battle': 1},
+    {'battle': 2},
     {'battle': 3},
     {'battle': 4, 'boss': 1},
 ]
@@ -47,87 +58,44 @@ A7, B7, C7, D7, E7, F7, G7, H7, I7, J7, K7, \
 A8, B8, C8, D8, E8, F8, G8, H8, I8, J8, K8, \
     = MAP.flatten()
 
-roads = [RoadGrids([D4, F5, G4, H3])]
-
 
 class Config(ConfigBase):
     # ===== Start of generated config =====
+    MAP_SIREN_TEMPLATE = ['0']
+    MOVABLE_ENEMY_TURN = (2,)
+    MAP_HAS_SIREN = True
+    MAP_HAS_MOVABLE_ENEMY = True
     MAP_HAS_MAP_STORY = False
     MAP_HAS_FLEET_STEP = False
     MAP_HAS_AMBUSH = True
-    # ===== End of generated config =====
-
-    INTERNAL_LINES_FIND_PEAKS_PARAMETERS = {
-        'height': (120, 255 - 17),
-        'width': (0.9, 10),
-        'prominence': 10,
-        'distance': 35,
-    }
-    EDGE_LINES_FIND_PEAKS_PARAMETERS = {
-        'height': (255 - 50, 255),
-        'prominence': 10,
-        'distance': 50,
-        'wlen': 1000
-    }
-    INTERNAL_LINES_HOUGHLINES_THRESHOLD = 25
-    EDGE_LINES_HOUGHLINES_THRESHOLD = 25
+    MAP_HAS_MYSTERY = True
+    MAP_CHAPTER_SWITCH_20241219 = True
+    STAGE_ENTRANCE = ['half', '20240725']
+    MAP_HAS_MODE_SWITCH = True
+    STAGE_INCREASE_AB = True
     MAP_WALK_USE_CURRENT_FLEET = True
+    # ===== End of generated config =====
 
 
 class Campaign(CampaignBase):
     MAP = MAP
-
-    def battle_default(self):
-        if self.clear_roadblocks(roads):
-            return True
-        if self.clear_potential_roadblocks(roads):
-            return True
-        if self.clear_filter_enemy(self.ENEMY_FILTER, preserve=1):
-            return True
-
-        return super().battle_default()
+    ENEMY_FILTER = '1L > 1M > 1E > 1C > 2L > 2M > 2E > 2C > 3L > 3M > 3E > 3C'
 
     def battle_0(self):
-        if self.clear_chosen_enemy(D4):
+        if self.clear_siren():
+            return True
+        if self.clear_filter_enemy(self.ENEMY_FILTER, preserve=5):
             return True
 
         return self.battle_default()
 
-    def battle_1(self):
-        if not self.map_is_clear_mode:
-            self.destroy_land_base(C1, D1, D1)
-
-        if self.clear_chosen_enemy(F5):
-            return True
-
-        return self.battle_default()
-
-    def battle_2(self):
-        if self.clear_chosen_enemy(G4):
-            return True
-
-        return self.battle_default()
-
-    def battle_3(self):
-        if self.clear_chosen_enemy(H3):
-            return True
-
-        return self.battle_default()
-
-    def battle_4(self):
-        if not self.map_is_clear_mode:
-            # destroy K8 and G8 land base in one air attack
-            self.destroy_land_base(K8, J6, I8)
-
-        boss = self.map.select(is_boss=True)
-        if boss:
-            return self.fleet_boss.brute_clear_boss()
-
-        if self.clear_roadblocks(roads):
-            return True
-        if self.clear_potential_roadblocks(roads):
+    def battle_5(self):
+        if self.clear_siren():
             return True
         if self.clear_filter_enemy(self.ENEMY_FILTER, preserve=0):
             return True
 
         return self.battle_default()
+
+    def battle_10(self):
+        return self.fleet_boss.clear_boss()
--- dev_tools/map_extractor.py
+++ dev_tools/map_extractor.py
@@ -745,10 +745,10 @@ Arguments:
     IS_WAR_ARCHIVES: True if retrieved map is to be
                      adapted for war_archives usage
 """
-FILE = '../AzurLaneLuaScripts'
+FILE = './AzurLaneLuaScripts'
 FOLDER = './campaign/test'
-KEYWORD = ''
-SELECT = True
+KEYWORD = '1604'
+SELECT = False
 OVERWRITE = True
 IS_WAR_ARCHIVES = False
 ENEMY_FILTER = '1L > 1M > 1E > 1C > 2L > 2M > 2E > 2C > 3L > 3M > 3E > 3C'
